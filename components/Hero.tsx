faa13b77b088b3115de310c09f0977bfa1d56bb4
Ali <malihumayon@gmail.com>
2024-08-15 14:38:00 +0500

    hero changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
               <TextGenerateEffect
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                words="Weaving concepts into the fabric of everyday life."
+                words="Full Stack Web Development"
               />
 
               <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
