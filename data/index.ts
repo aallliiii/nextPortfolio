64d2b11dfdc9564b0a8bc2de83e38edb42d6e828
Ali <malihumayon@gmail.com>
2024-08-29 19:22:18 +0500

    Project Added

--- data/index.ts
+++ data/index.ts
@@ -9,6 +9,14 @@ export const MyProjectsData = [
   },
   {
     id: 2,
+    title: "Shopease (Ecommerce Website)",
+    description:
+      "ShopEase is a modern, user-friendly e-commerce platform built using the MERN stack (MongoDB, Express.js, React, and Node.js) with Redux for state management. Designed to enhance the shopping experience, ShopEase features a sleek interface, responsive design, and seamless navigation. It integrates various payment gateways and supports multiple product categories, providing a comprehensive solution for both small and large-scale businesses. With robust search capabilities, personalized recommendations, and a secure checkout process, ShopEase ensures a smooth and efficient shopping experience for all users.",
+    img: "/shopease.png",
+    link: "https://www.linkedin.com/posts/muhammad-ali-701450214_mernstack-ecommercewebsite-webdevelopment-activity-7222601677428670464-5D8r?utm_source=share&utm_medium=member_desktop",
+  },
+  {
+    id: 3,
     title: "My Weather App",
     description:
       "This Node.js and Express-based weather application leverages a public weather API to deliver real-time weather updates for any location. Users can input a city name to receive up-to-date information on temperature, humidity, and weather conditions. This project demonstrates my proficiency in integrating third-party APIs with server-side JavaScript frameworks, showcasing my skills in backend development and my commitment to creating practical, user-friendly applications.",
@@ -16,7 +24,7 @@ export const MyProjectsData = [
     link: "https://github.com/aallliiii/Weather-App.git",
   },
   {
-    id: 3,
+    id: 4,
     title: "Book Tracking App",
     description:
       "I developed a comprehensive book tracking app to keep a detailed record of the books I read, utilizing Node.js, Express, and EJS. The application integrates with a public API to fetch book cover images, enhancing the visual appeal and user experience. Users can add, update, and delete book entries, with each entry displaying the book title, author, and cover image. This project demonstrates my proficiency in backend development, templating with EJS, and seamless API integration, reflecting my ability to build practical, user-centric applications.",
@@ -24,7 +32,7 @@ export const MyProjectsData = [
     link: "https://github.com/aallliiii/Book-Review-WebApp.git",
   },
   {
-    id: 4,
+    id: 5,
     title: "Windows form Gym Management System",
     description:
       "The advanced Gym Management System is a robust application designed to streamline fitness center operations. It integrates database functionality and file handling, offering both Windows Forms and console-based interfaces. Utilizing object-oriented programming (OOP), the system ensures modularity and maintainability. It features distinct roles for members, trainers, and administrators: members can view and select workout routines and diet plans, trainers can manage and customize these plans, and administrators handle registrations, scheduling, and billing. These enhancements provide efficient and user-friendly management for small to medium-sized fitness centers.",
@@ -32,7 +40,7 @@ export const MyProjectsData = [
     link: "https://github.com/aallliiii/OopApp.git",
   },
   {
-    id: 4,
+    id: 6,
     title: "Console based Gym Management System",
     description:
       "The console-based Gym Management System is designed to streamline the operations of a fitness center by offering distinct roles for members, trainers, and administrators. Members can view and select personalized workout routines and diet plans curated by trainers, and track their progress. Trainers have the ability to create, read, update, and delete (CRUD) workout plans and diet schedules tailored to individual member needs. Administrators oversee and manage all aspects of the system, including member registrations, trainer assignments, class schedules, and billing operations. With an intuitive command-line interface, this system ensures efficient and user-friendly management of daily gym activities, making it an ideal solution for small to medium-sized fitness centers.",
