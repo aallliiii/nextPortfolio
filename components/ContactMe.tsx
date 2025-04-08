38518e43a198a34d370769a9b871ab5165bdc5cc
aallliiii <malihumayon@gmail.com>
2025-04-09 00:56:39 +0500

    Some Changes

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -27,7 +27,7 @@ export function ContactMe() {
               type="button"
               className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg "
             >
-              Get my LinkedIn
+              LinkedIn
             </button>
           </Link>
           <CopyButtonGmail />
